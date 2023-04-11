619443b368440db67020624f404970d3a1cfb8b5
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-04-11 21:08:44 +0100

    set up animes page-type

---
+++ anime/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class AnimePage(Page):
+    # page_description = "New anime listing"
+    anime_banner_img = models.ImageField()
+    anime_title = models.CharField(max_length=300, blank=False)
+    anime_description = RichTextField(blank=True)
+    # tags = models.Choices([("COMEDY",
+    #                        "DRAMA",
+    #                         "SHONEN",
+    #                         "ACTION",
+    #                         "ADVENTURE",
+    #                         "SUPERNATURAL",)])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('anime_banner_img'),
+        FieldPanel('anime_title'),
+        FieldPanel('anime_description'),
+        # FieldPanel('tags', classname="full"),
+    ]
--- backend/settings/base.py
+++ backend/settings/base.py
@@ -26,6 +26,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     "home",
     "search",
+    "anime",
+    "wagtail.api.v2",
+    "rest_framework",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
