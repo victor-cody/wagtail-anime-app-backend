d7a838ab32bf7c473cc3de8aea68f49569d598cf
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-04-15 23:53:34 +0100

    DEBUG

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -1,10 +1,15 @@
+import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wciltrm*7p&@fk-#ajm1@h3(xn+0#zby_$3(qopu0h876*++9="
+# SECRET_KEY = "django-insecure-wciltrm*7p&@fk-#ajm1@h3(xn+0#zby_$3(qopu0h876*++9="
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', default='django-insecure-wciltrm*7p&@fk-#ajm1@h3(xn+0#zby_$3(qopu0h876*++9=')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
