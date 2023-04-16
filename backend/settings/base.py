15530b01f62f10bd6bd8dbad09f544baebdb465e
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-04-16 19:39:03 +0100

    fixes for deployment to Railway 🚊 - 2

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -90,18 +90,18 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/backend',
-        conn_max_age=600)}
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://postgres:postgres@localhost:5432/backend',
+#         conn_max_age=600)}
 
 
 # Password validation
